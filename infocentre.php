9dd8dc577dc9f034ce226080fa5407437a8808e9
Liveapps <nagendra@bramblebusiness.com>
2013-10-24 10:32:11 +0530

    Create infocentre.php

---
+++ infocentre.php
@@ -0,0 +1,105 @@
+<?php 
+
+ include("include/functions.php");
+
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>WEEE Registration, WEEE De Minimis Registration, WEEE Changes 2014</title>
+<meta name="description" content="WEEE 2013 Online De Minimis Registration Form - A Simpler Compliant Future">
+<meta name="keywords" content="WEEE Registration, WEEE De minimis Regulations, WEEE Changes 2014, WEEE Reg, WEEE Reg 2014, WEEE Reg 2015">
+<link rel="stylesheet" href="css/wee_style.css"/>
+<link rel="stylesheet" href="css/style.css"/>
+</head>
+
+<body>
+<div style="max-width:980px; margin-right:auto; margin-left:auto; overflow:hidden;">
+<!-- Header start-->
+<?php top();
+?>
+<!-- header close-->
+
+ <div>
+ <?php sidemenu(); ?>
+<div style="width:800px;">
+<div><strong style="font-size: 22px;"><h1><span style="color:#2141c2;">WEEE</span><span style=" color:#140033;"> Info Centre</h1></strong>
+<div><span style="font-size: 12px;color:#140033; text-align:justify;"></span></div>
+</div><br />
+
+<div class="style1" style=" height:auto; background-color:#aaafc2; padding: 10px 0px 10px 10px;width: 755px;">
+ <div class="style2"><strong  style="font-weight: bold;font-size: 18px;color:#140033;">Department for Business Innovation and Skills</strong></div>
+</div>
+<br />
+<br />
+<br />
+
+
+<div style="font-size: 12px; margin-top:17px ;">
+<div width="980" border="1">
+
+
+<div width="1024"> 
+
+<div style="float:left;width:200px;"><span style="color:#151663; font-family:verdana;font-weight: bold;">Draft Statutory Instrument <br />
+WEEE Regs 2013</span><br /><br />
+<img src="images/1.png" style="border: 3px solid #151663;"  />
+<a href="infocentre_pdf/bis-13-1179-government-response-waste-electrical-and-electronic-equipment-weee-implementing-the-recast-directive-and-uk-system-changes.pdf" target="_blank"><button class="button_base" style="border-radius: 5PX;color: #FFF;padding: 5px 5px 5px 5px;cursor: pointer;border-style: none;margin-left: 34px;" >Download PDF</button></a>
+</div>
+
+
+<div style="float:left;width:200px;"><span style="color:#151663; font-family:verdana;font-weight: bold;">Impact Assessment<br />
+ WEEE System</span><br /><br />
+<img src="images/2.png" style="border: 3px solid #151663;" />
+<a href="infocentre_pdf/bis-13-1181-impact-assessment-waste-electrical-and-electronic-equipment-weee-system.pdf" target="_blank"><button class="button_base" style="border-radius: 5PX;color: #FFF;padding: 5px 5px 5px 5px;cursor: pointer;border-style: none;margin-left: 36px;" >Download PDF</button></a>
+</div>
+
+<div style="float:left;width:200px;"><span style="color:#151663; font-family:verdana;font-weight: bold;">Impact Assessment Recast<br />
+ Directive-201219E WEEE</span><br /><br />
+<img src="images/3.png" style="border: 3px solid #151663;" />
+<a href="infocentre_pdf/bis-13-1180-impact-assessment-recast-directive-201219EU-waste-electrical-and-electronic-equipment-weee.pdf"target="_blank">
+<button class="button_base" style="border-radius: 5PX;color: #FFF;padding: 5px 5px 5px 5px;cursor: pointer;border-style: none;margin-left: 36px;margin-top: 5px;" >Download PDF</button></a>
+</div>
+
+<div style="float:left;width:200px;margin-top: -29px;"><span style="color:#151663; font-family:verdana;font-weight: bold;">Government Response<br />
+WEEE Implementing <br />
+The Recast Dirctive <br />
+and UK System Changes </span> <br /><br />
+
+<img src="images/4.png" style="border: 3px solid #151663;" />
+<a href="infocentre_pdf/bis-13-1182-draft-statutory-instrument-waste-electrical-and-electronic-equipment-regulations-2013.pdf" target="_blank"><button class="button_base" style="border-radius: 5PX;color: #FFF;padding: 5px 5px 5px 5px;cursor: pointer;border-style: none;margin-left: 36px;" >Download PDF</button></a>
+ 
+</form>
+</div>
+
+</div>
+    
+       
+       
+  
+</div>
+</div>
+
+
+</div>
+</div></div>
+
+<!--<div id="footer">
+<span style="font-size:12px; color:#000033; line-height:40px; padding-left:2px;">
+&copy;Copyright 2012 Weeeindia All Rights Reserved 
+</span>
+<span style=" float:right;font-size:14px; color:#7dbd2b; line-height:40px; padding-right:2px;">
+Home &nbsp;|&nbsp; Sitemap &nbsp;|&nbsp; Privacy &nbsp;| &nbsp;eee register &nbsp;| &nbsp;Contact Us  
+</span>
+<br/>
+</div>--><br />
+<br /><br />
+<br />
+
+
+<?php
+footer();
+?>
+</body>
+</html>
