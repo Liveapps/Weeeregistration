2a43a7005763a33ad10607fd725556afdec338d7
Liveapps <nagendra@bramblebusiness.com>
2013-10-24 10:29:36 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+<span style="font-size:12px; font-family:arial; color:#000033; line-height:40px; padding-left:2px;">
+&copy;Copyright 2013 Weeeregistration All Rights Reserved 
+</span>
+<span style=" float:right;font-size:14px; line-height:40px; padding-right:2px; font-family:arial;">
+<a class="footer_menu" href="http://weeeregistration.org/">Home</a> &nbsp;|&nbsp; <a class="footer_menu" href="/contactus.php">Contact Us </a> 
+</span>
+<br/>
+</div>
