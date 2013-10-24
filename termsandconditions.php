8953de5165d3209fe57c81364efb1b61d3f1e14e
Liveapps <nagendra@bramblebusiness.com>
2013-10-24 10:40:42 +0530

    Create termsandconditions.php

---
+++ termsandconditions.php
@@ -0,0 +1,120 @@
+<?php 
+
+ include("include/functions.php");
+
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>WEEE Registration, WEEE De Minimis Registration, WEEE Changes 2014</title>
+<meta name="description" content="WEEE 2013 Online De Minimis Registration Form - A Simpler Compliant Future">
+<meta name="keywords" content="WEEE Registration, WEEE De minimis Regulations, WEEE Changes 2014, WEEE Reg, WEEE Reg 2014, WEEE Reg 2015">
+<link rel="stylesheet" href="css/wee_style.css"/>
+<link rel="stylesheet" href="css/style.css"/>
+</head>
+
+<body>
+<!-- Header start-->
+<?php top(); ?>
+<!-- header close-->
+<!-- Main Content start-->
+
+
+<!-- Navigation -->
+
+
+
+ <div >
+<div style="width:721px; overflow: hidden;">
+<div><div style="margin-top: 28px;
+margin-bottom: -11px; float:left;"><strong style="font-family: arial Black;
+font-size: 22px;"><span style="color:#2141c2;">WEEE</span color:#140033;> Privacy Policy<br /></strong>
+<br /><div style="float:right;float: right;
+margin-right: -191%;
+margin-top: -12%;">
+<span>
+<input type="button"  value="Print Page" class="button_base" style="width: 100%;height: 3%;color: #ffffff; border:0px; border-radius: 3px; cursor:pointer;margin-top:-10px;" onClick="window.print()" ></span></div>
+
+<!--<div><span style="font-family: arial;
+font-size: 12px;color:#140033; text-align:justify;">
+</span></div>-->
+</div></div><br />
+
+<div class="style1" style=" height:auto; background-color:#aaafc2; padding: 10px 0px 10px 10px; ">
+ <div class="style2"><strong  style=" font-family: arial Black;font-weight: bold;font-size: 18px;color:#140033;">Cookie Policy</strong></div>
+</div>
+<div style="font-size: 12px; margin-top:20px ;margin-bottom: -60px;">
+<span style="font-family: arial;
+font-size: 12px;color:#140033;">By browsing the WEEERegistration.org website you agree that this website may store Cookies on your computer for the purpose of providing our online products and services to you. You may refuse to accept the use of Cookies by disabling Cookies from your browser settings. However doing this may disable certain functions of this website.
+</span>
+<br /><br />
+<span style="font-weight:bold;">What is a cookie?</span><br /><br />
+<span>
+A cookie is a browser specific alphanumeric key to identify your browser. This is typically used for such things as remembering that you are logged into a particular website so that you are not required to fill in your information every time that you visit. Cookies typically last the time that your browser is kept open and are deleted once you close it. These are called Session cookies. Cookies can be stored on your browser for a longer period of time even upon closing your browser. These are called Persistent Cookies. To provide the best experience for you we may use a variety of both.
+</span></div>
+<br />
+
+<div class="style1" style=" height:auto; background-color:#aaafc2; padding: 10px 0px 10px 10px; margin-top:10%; ">
+ <div class="style2"><strong  style=" font-family: arial Black;font-weight: bold;font-size: 18px;color:#140033;">Privacy Policy</strong></div>
+</div>
+<div style="font-size: 12px; margin-top:20px ;margin-bottom: -60px;font-family: arial ;color:#140033;">
+<span>
+We take your Privacy serious. All Data Collected will be used strictly within the framework of the Data Protection Act 1998. This includes but is not limited to that:</span><br />
+<span>- We will not give your data to any 3rd party without your explicit consent
+</span><br />
+<span>- We will not store your data for longer than is reasonably required for us to provide you our products and services
+</span><br />
+<span>- We will take measures to keep your data secure
+</span><br />
+<span>- We will where necessary keep your information up to date
+</span><br /><br />
+<span>Non-Personally Identifiable information may be collected and may be shared to 3rd parties for the purpose of continuing to provide you with our high level of service. Such 3rd parties may include but are not limited to Google Analytics (website traffic monitoring).
+
+
+</span>
+
+
+
+</div>
+
+
+</div></div>
+
+
+
+
+
+<div style="float: right;margin-top: -416px;">
+
+<?php sidemenu(); ?>
+</div>
+
+<!--<div style="width:720px;border-bottom:3px solid #aaafc2;">
+</div>-->
+<!--content End-->
+<div style="clear:both;"></div>
+
+<div style="clear:both;"> </div>
+
+
+
+
+<!-- main content close-->
+<br/>
+
+<!--<div id="footer">
+<span style="font-size:12px; color:#000033; line-height:40px; padding-left:2px;">
+&copy;Copyright 2012 Weeeindia All Rights Reserved 
+</span>
+<span style=" float:right;font-size:14px; color:#7dbd2b; line-height:40px; padding-right:2px;">
+Home &nbsp;|&nbsp; Sitemap &nbsp;|&nbsp; Privacy &nbsp;| &nbsp;eee register &nbsp;| &nbsp;Contact Us  
+</span>
+<br/>
+</div>-->
+
+<?php
+footer();
+?>
+</body>
+</html>
