495f30071884392728c961d8e980899ebcca50b4
Liveapps <nagendra@bramblebusiness.com>
2013-10-24 10:39:43 +0530

    Create sucesses.php

---
+++ sucesses.php
@@ -0,0 +1,51 @@
+<?php 
+
+ include("include/functions.php");
+
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>WEEE Registration, WEEE De Minimis Registration, WEEE Changes 2014</title>
+<meta name="description" content="WEEE 2013 Online De Minimis Registration Form - A Simpler Compliant Future">
+<meta name="keywords" content="WEEE Registration, WEEE De minimis Regulations, WEEE Changes 2014, WEEE Reg, WEEE Reg 2014, WEEE Reg 2015">
+<link rel="stylesheet" href="css/wee_style.css"/>
+<link rel="stylesheet" href="css/style.css"/>
+</head>
+
+<body>
+<div style="max-width:980px; margin-right:auto; margin-left:auto; overflow:hidden;">
+<!-- Header start-->
+<?php top(); ?>
+<!-- header close-->
+
+ <div>
+ <?php sidemenu(); ?>
+<div style="width:721px;">
+<div><strong style="font-family: arial Black;font-size: 22px;"><h1><span style="color:#2141c2;">WEEE</span><span color:#140033;> Registration Form</span></h1></strong><br />
+
+<div><span style="font-family: arial;font-size: 12px;color:#140033; text-align:justify;"></span></div>
+</div>
+
+
+<div style="font-size: 12px; margin-top:20px ;">
+<span style="font-family: arial;
+font-size: 12px;color:#140033;">Thank you for your registration details for WEEE Compliance. Your details have been successfully stored within our system and a member of our WEEE Registration team will be in contact with you shortly by phone. In the meantime if you have any queries or questions please don’t hesitate to <a href="contactus.php">contact us</a>.</span></div>
+<br />
+<div style=" border-bottom:3px solid #aaafc2;">
+</div>
+<!--content End-->
+</div>
+</div></div></div>
+
+<!-- main content close-->
+<br/>
+<br/><br />
+
+
+<br/><br/>
+
+</body>
+</html>
+<?php include("footer.php");?>
